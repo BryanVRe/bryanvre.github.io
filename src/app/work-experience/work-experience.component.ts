5ad939f80a43b762526643929ae28a2e3554626e
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-08-30 12:47:46 -0500

    create work_experience

--- src/app/work-experience/work-experience.component.ts
+++ src/app/work-experience/work-experience.component.ts
@@ -7,9 +7,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class WorkExperienceComponent implements OnInit {
 
+  workExperiencies : Array<any> = [];
+
   constructor() { }
 
   ngOnInit(): void {
+
+    let work1 = {
+      fecha: "2015-2018",
+      ubicacion : "Orizaba, Ver",
+      puesto:"CEO",
+      empresa:"Waves",
+      logros:[
+        {descripcion: "Dron autonomo"},
+        {descripcion:"Cluster GPU"},
+
+      ]
+    };
+    let work2 = {
+      fecha:"2015-2018",
+      ubicacion:"Ixtac, Ver",
+      puesto: "CEO",
+      empresa:"Kubbet",
+      logros:[
+        {descripcion: "Cluster hadoop"},
+        {descripcion:"Red Neuronal GAN"},
+
+      ]
+    };
+    this.workExperiencies.push(work1);
+    this.workExperiencies.push(work2);
   }
 
 }
