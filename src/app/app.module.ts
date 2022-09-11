d0c8b07b3eb6455ac3f7d4a21f48c910b7062abb
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-09-11 13:38:42 -0500

    update Languages #1

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,12 +4,14 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { LanguagesComponent } from './languages/languages.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
-    WorkExperienceComponent
+    WorkExperienceComponent,
+    LanguagesComponent
   ],
   imports: [
     BrowserModule
