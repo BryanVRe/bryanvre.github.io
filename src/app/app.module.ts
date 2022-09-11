ff14ded5d9d424a75608a65e4b236cec5ea2fbd3
Bryan Valerio Reyes <fsocietybvr@gmail.com>
2022-09-11 15:05:10 -0500

    upd

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,8 +4,11 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './header/header.component';
 import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { EducationComponent } from './education/education.component';
+import { SkillComponent } from './skill/skill.component';
+import { AyCComponent } from './ay-c/ay-c.component';
+import { LanguagesComponent } from './languages/languages.component';
 import { InterestsComponent } from './interests/interests.component';
-
 
 @NgModule({
   declarations: [
